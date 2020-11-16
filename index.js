639ba680e42a231d14998e8b5dd5bb6595af4062
Noah Franklin <noahfranklin@Noahs-MacBook-Pro.local>
2020-11-15 21:14:35 -0500

    heroku plsssss

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ app.use(cors());
 app.use('/users', users);
 
 
-const port = process.env.API_PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
