8ad3f80c5038386e37614d82ff184c14ec019432
Noah Franklin <noahfranklin@Noahs-MacBook-Pro.local>
2020-11-21 14:12:53 -0500

    added password hashing

--- models/register.js
+++ models/register.js
@@ -1,8 +1,13 @@
 const db = require('../services/db.js');
-const prefix = "EX_Fall_2020_"
-
+const prefix = "EX_Fall_2020_";
+const bcrypt = require('bcrypt');
 module.exports = {
     addUser: (req,res) => {
+        res.body.Password = bcrypt.genSalt(process.env.SALT, (err, salt) => {
+            bcrypt.hash(res.body.Password, salt, (err , hash) => {
+                return hash;
+            })
+        })
         db.query(`INSERT INTO ${prefix}Users (created_at, FirstName, LastName, DOB, Password, User_Type) 
         VALUES ('${req.body.created_at}', '${req.body.FirstName}', '${req.body.LastName}', '${req.body.DOB}', '${req.body.Password}', '${req.body.User_Type}')`, 
         (err, results, fields) => {
