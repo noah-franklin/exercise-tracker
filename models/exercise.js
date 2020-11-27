b17c26b1bf3565be669b928daa7cb489b568e290
Noah Franklin <noahfranklin@Noahs-MacBook-Pro.local>
2020-11-26 22:53:56 -0500

    added exercise functionality

--- models/exercise.js
+++ models/exercise.js
@@ -12,5 +12,63 @@ module.exports = {
         });
             
 
+    },
+    addExercise: (req, res) => {
+        let token = req.headers.authorization.split(' ')
+            //console.log(token[1])
+            let decoded = jwt.verify(token[1], process.env.JWT_SECRET);
+            decoded = JSON.parse(decoded.data)
+        if(decoded[0].User_Type == 1){
+            
+            //console.log('wtf')
+            db.query(`INSERT INTO ${prefix}Exercise_Types (created_at, Name, Type) 
+            VALUES ('${req.body.created_at}', '${req.body.Name}', '${req.body.Type}')`, 
+            (err, results, fields) => {
+            if (err) throw err
+            res.send("Successfully added exercise!")
+        })
+        }
+        
+
+    },
+    deleteExercise: (req, res) => {
+        //console.log(req.params.id)
+        //console.log(req.headers.authorization)
+        let token = req.headers.authorization.split(' ')
+        //console.log(token[1])
+        let decoded = jwt.verify(token[1], process.env.JWT_SECRET);
+        decoded = JSON.parse(decoded.data)
+        //console.log(decoded[0].User_Type)
+        //res.send("found me lol")
+        if(decoded[0].User_Type == 1){
+            //console.log('wtf')
+            db.query(`DELETE FROM ${prefix}Exercise_Types WHERE id = ${req.params.id}`,
+            (err, results, fields) => {
+                if (err) throw err
+               
+            })
+        }
+
+    },
+    editExercise: (req, res) => {
+        //console.log(req.params.id)
+        //console.log(req.headers.authorization)
+        let token = req.headers.authorization.split(' ')
+        //console.log(token[1])
+        let decoded = jwt.verify(token[1], process.env.JWT_SECRET);
+        decoded = JSON.parse(decoded.data)
+        //console.log(decoded[0].User_Type)
+        //res.send("found me lol")
+        if(decoded[0].User_Type == 1){
+            //console.log('wtf')
+            db.query(`UPDATE ${prefix}Exercise_Types 
+            SET Name = '${req.body.Name}',
+            Type = '${req.body.Type}'
+            WHERE id = ${req.params.id}`,
+            (err, results, fields) => {
+                if (err) throw err
+                
+            })
+        }
     }
 }
--- routes/exercise.js
+++ routes/exercise.js
@@ -5,6 +5,8 @@ const models = require('../models/exercise.js');
 
 
 router.get('/', models.getAllExercise);
-
+router.get('/:id/delete', models.deleteExercise);
+router.post('/:id/update', models.editExercise);
+router.post('/', models.addExercise);
 
 module.exports = router;
