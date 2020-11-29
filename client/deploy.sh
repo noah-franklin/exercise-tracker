4bd5e64e9cd7736d5b55bb88f0a3ced33d0d8599
Noah Franklin <noahfranklin@Noahs-MacBook-Pro.local>
2020-11-29 18:03:41 -0500

    trying heroku

--- client/deploy.sh
+++ client/deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:SoGucci/SoGucci.github.io.git main
+git push -f https://github.com/SoGucci/SoGucci.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
